edeb8f833dbe436352c4e6d103da88cec6c33c96
김지윤 <kimjiyoon@pubg.com>
2026-07-12 22:30:26 +0900

    1장: 웹페이지 다운로드

---
+++ parse-url/browser.py
@@ -0,0 +1,72 @@
+import socket
+import ssl
+
+class URL:
+    def __init__(self, url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https"]
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+        self.port = None
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+    def request(self):
+        if self.port is None:
+            if self.scheme == "http":
+                self.port = 80
+            else:
+                self.port = 443
+
+        sock = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            sock = ctx.wrap_socket(sock, server_hostname=self.host)
+        sock.connect((self.host, self.port)) 
+
+        req = "GET {} HTTP/1.0\r\n".format(self.path)
+        req += "Host: {}\r\n".format(self.host)
+        req += "\r\n"
+        sock.send(req.encode("utf8"))
+        res = sock.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = res.readline()
+        version, status, explanation = statusline.split(" ",2)
+        res_headers = {}
+        while True:
+            line = res.readline()
+            if line == "\r\n": break
+            header, value = line.split(":", 1)
+            res_headers[header.casefold()] = value.strip()
+
+            assert "transfer-encoding" not in res_headers
+            assert "content-encoding" not in res_headers
+
+            body = res.read()
+            sock.close()
+            return body
+
+def show(body):
+        in_tag = False
+        for c in body:
+            if c == "<":
+                in_tag = True
+            elif c == ">":
+                in_tag = False
+            elif not in_tag:
+                print(c, end="")
+def load(url):
+        body = url.request()
+        show(body)
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
+
+
